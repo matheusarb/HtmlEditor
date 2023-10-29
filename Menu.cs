0d620862dcc255a0bab06a83c852e21f6f7cc74d
Matheus <mat.araujoribeiro@gmail.com>
2023-10-29 19:26:37 -0300

    [add] HandleMenuOption

--- Menu.cs
+++ Menu.cs
@@ -11,6 +11,10 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
 
             DrawScreen();
+            WriteOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
         }
 
         public static void DrawScreen()
@@ -41,6 +45,44 @@ namespace EditorHtml
                     }
                 Console.WriteLine("|");
                 //Console.Write("\n");
+            }
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma das opções abaixo");
+
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+
+            Console.SetCursorPosition(3, 9);
+            Console.WriteLine("0 - Sair");
+
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+        }
+
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Console.WriteLine("Editor"); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0: 
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+                default: Show(); break;
             }
         }
     }
